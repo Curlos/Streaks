c2b0d051d1eccd232337be6038d2e1fec0936bef
Carlos Martinez <curlosmart@gmail.com>
2021-08-31 13:40:36 -0400

    base setup

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import Tasks from './components/Tasks'
+import './style.css';
+
+function App() {
+
+  const [tasks, setTasks] = useState([{
+    title: '',
+    icon: '',
+    color: '',
+    measurementDuration: '',
+    taskDays: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    numberOfDaysPerWeek: 7,
+    timesADay: 1,
+    timesDoneToday: 0,
+    currentStreak: 0,
+    completed: false,
+  }])
+
+  return (
+    <div className="App">
+      Streaks App
+      <Tasks />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Tasks.js
@@ -0,0 +1,11 @@
+import Task from './Task'
+
+const Tasks = () => {
+  return (
+    <div>
+      hello
+    </div>
+  )
+}
+
+export default Tasks;
