e4730bf04441b94a41439022711f5c48eceb0549
Carlos Martinez <curlosmart@gmail.com>
2021-08-31 21:23:29 -0400

    fixed unused var for deployment

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { useState } from 'react'
+//import React, { useState } from 'react'
 import Tasks from './components/Tasks'
 import './style.css';
 
 function App() {
 
-  const [tasks, setTasks] = useState([{
-    title: '',
-    icon: '',
-    color: '',
-    measurementDuration: '',
-    taskDays: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-    numberOfDaysPerWeek: 7,
-    timesADay: 1,
-    timesDoneToday: 0,
-    currentStreak: 0,
-    completed: false,
-  }])
+  // const [tasks, setTasks] = useState([{
+  //   title: '',
+  //   icon: '',
+  //   color: '',
+  //   measurementDuration: '',
+  //   taskDays: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+  //   numberOfDaysPerWeek: 7,
+  //   timesADay: 1,
+  //   timesDoneToday: 0,
+  //   currentStreak: 0,
+  //   completed: false,
+  // }])
 
   return (
     <div className="App">
