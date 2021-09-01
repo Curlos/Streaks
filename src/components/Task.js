ebc1f31e7c0aed4f8bf21945de47589d23fd5034
Carlos Martinez <curlosmart@gmail.com>
2021-08-31 21:19:13 -0400

    simple task component created

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,5 +1,13 @@
 const Task = () => {
-
+  
+  return (
+    <div className="task">
+      <div className="taskCircle">
+        <div className="taskIcon"><i class="far fa-clock fa-6x"></i></div>
+      </div>
+      <div className="taskName">Focus for 6 hours</div>
+    </div>
+  )
 }
 
 export default Task;
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -2,8 +2,21 @@ import Task from './Task'
 
 const Tasks = () => {
   return (
-    <div>
-      hello
+    <div className="tasksContainer">
+      <div className="tasks">
+        <Task />
+        <Task />
+        <Task />
+        <Task />
+        <Task />
+        <Task />
+        <Task />
+        <Task />
+        <Task />
+        <Task />
+        <Task />
+        <Task />
+      </div>
     </div>
   )
 }
