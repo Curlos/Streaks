2ce5f6fa0638d523b31ea18ccd0e1a5cf5b648d7
Carlos Martinez <curlosmart@gmail.com>
2021-09-09 09:30:45 -0400

    nuked project

--- src/App.js
+++ src/App.js
@@ -1,39 +1,13 @@
 import React, { useState } from 'react'
-import Tasks from './components/Tasks'
-import AddNewTaskButton from './components/AddTaskComponents/AddNewTaskButton'
-import NewTaskForm from './components/AddTaskComponents/NewTaskForm'
 import './style.css';
 
 const App = () => {
 
-  const [tasks, setTasks] = useState([{
-    title: '',
-    icon: '',
-    color: '',
-    measurementDuration: '',
-    taskDays: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-    numberOfDaysPerWeek: 7,
-    timesADay: 1,
-    timesDoneToday: 0,
-    currentStreak: 0,
-    completed: false,
-  }])
-  const [showNewTaskForm, setShowNewTaskForm] = useState(true)
-
-  const toggleModal = () => {
-    setShowNewTaskForm(!showNewTaskForm)
-  }
-
-  const handleNewTaskFormSubmit = (newTask) => {
-    setTasks([...tasks, newTask])
-  }
 
   return (
     <div className="App">
       Streaks App
-      <Tasks />
-      <AddNewTaskButton toggleModal={toggleModal}/>
-      {showNewTaskForm ? <NewTaskForm toggleModal={toggleModal} handleNewTaskFormSubmit={handleNewTaskFormSubmit}/> : null}
+      
     </div>
   );
 }
